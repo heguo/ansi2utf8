5b486c4d56f7b91fe5c2f0dc89331e4f4ccd1157
shuozi <shuozi@outlook.com>
2016-01-20 20:43:57 +0800

    publish

---
+++ ansi2utf8/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ansi2utf8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length == 2)
+            {
+                convert(args[0], args[1]);
+            }
+            else
+            {
+                Console.WriteLine("usage: convert \"c:\\ansifile.txt\" \"c:\\utf8file.txt\" ");
+            }
+        }
+
+        static void convert(string infile, string outfile)
+        {
+            if (!File.Exists(infile))
+            {
+                Console.WriteLine(string.Format("error: file {0} not found.", infile));
+                return;
+            }
+
+            if (!System.IO.Directory.Exists(System.IO.Path.GetDirectoryName(outfile)))
+            {
+                CreateDictory(System.IO.Path.GetDirectoryName(outfile));
+            }
+
+            try
+            {
+                var s = File.ReadAllText(infile,Encoding.Default);
+                using (var sw = new StreamWriter(outfile, false, Encoding.UTF8))
+                {
+                    sw.WriteLine(s);
+                    sw.Close();
+                }
+                Console.WriteLine(string.Format("success: {0} => {1}", infile,outfile));
+            }
+            catch
+            {
+                Console.WriteLine(string.Format("error: {0} => {1}", infile,outfile));
+            }
+        }
+
+        static void CreateDictory(string dict)
+        {
+            var parent = System.IO.Path.GetDirectoryName(dict);
+            if (!System.IO.Directory.Exists(parent))
+            {
+                CreateDictory(parent);
+                System.IO.Directory.CreateDirectory(dict);
+            }
+            else
+            {
+                System.IO.Directory.CreateDirectory(dict);
+            }
+        }
+    }
+}
---
+++ ansi2utf8/readme.txt
@@ -0,0 +1,25 @@
+﻿usage:
+
+convert.exe   "c:\\ansifile.txt"  "c:\utf8file.txt"
+
+ 
+
+more:
+
+del d:\a.txt
+cd D:\ansi2utf8\bin\Debug\
+d:
+ansi2utf8.exe "D:\Project\nav.html" "D:\Project2\nav.html" >> d:\a.txt
+ansi2utf8.exe "D:\Project\tpl\plan.html" "D:\Project2\tpl\plan.html" >> d:\a.txt
+ansi2utf8.exe "D:\Project\grid.js" "C:\Users\heguo\Desktop\utf8.txt" >> d:\a.txt
+
+notepad "d:\a.txt"
+del d:\a.txt
+
+------------------------------
+vs2015
+.net 4.5.2
+windows console program
+windows 10
+
+---------------------
